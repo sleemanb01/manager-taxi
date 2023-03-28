1d6c7c822d48bf3f6a5a1bf51fff6f599a64c47b
sleemanb01 <sleeman.nabwani@gmail.com>
2023-03-28 22:31:28 +0300

    v5.0 not tested

--- manager-taxi/src/pages/Auth.tsx
+++ manager-taxi/src/pages/Auth.tsx
@@ -1,28 +1,19 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, SafeAreaView, TextInput, StyleSheet } from "react-native";
+import { AuthContext } from "../hooks/auth-context";
+import { ENDPOINT_LOGIN, DEFAULT_HEADERS } from "../util/constants";
+import { useHttpClient } from "../hooks/http-hook";
 import { ErrorModal } from "../components/util/ErrorModal";
 import { Loading } from "../components/util/Loading";
-import { AuthContext } from "../hooks/auth-context";
-import { useHttpClient } from "../hooks/http-hook";
-import { DEFAULT_HEADERS, ENDPOINT_LOGIN } from "../util/constants";
-/* ************************************************************************************************** */
+import auth from "@react-native-firebase/auth";
 
 export default function Auth() {
+  const { login } = React.useContext(AuthContext);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [confirm, setConfirm] = React.useState(null);
+  const [code, setCode] = React.useState("");
 
-  const { login, logout } = React.useContext(AuthContext);
-  // const [formState, inputHandler, setFormData] = useForm(
-  //   {
-  //     code: reducerInputStateInitVal,
-  //     password: reducerInputStateInitVal,
-  //   },
-  //   false
-  // );
-
-  /* ************************************************************************************************** */
-
-  const authSubmitHandler = async () => {
+  const onAuthStateChanged = async () => {
     let TEST = { phone: "0548879522" };
     try {
       let res = await sendRequest(
@@ -36,16 +27,50 @@ export default function Auth() {
   };
 
   React.useEffect(() => {
-    authSubmitHandler();
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
   }, []);
 
-  /* ************************************************************************************************** */
+  async function signInWithPhoneNumber(phoneNumber: string) {
+    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+    console.log(confirmation);
+
+    // setConfirm(confirmation);
+  }
+
+  async function confirmCode() {
+    try {
+      // await confirm.confirm(code);
+    } catch (error) {
+      console.log("Invalid code.");
+    }
+  }
+
+  function confirmComponent() {
+    return (
+      <React.Fragment>
+        <TextInput value={code} onChangeText={(text) => setCode(text)} />
+        <Button title="Confirm Code" onPress={() => confirmCode()} />
+      </React.Fragment>
+    );
+  }
+
+  function notConfirmComponent() {
+    return (
+      <React.Fragment>
+        <Button
+          title="Phone Number Sign In"
+          onPress={() => signInWithPhoneNumber("+972548879522")}
+        />
+      </React.Fragment>
+    );
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <ErrorModal error={error} onClear={clearError} />
       {isLoading && <Loading />}
-      <Text> Auth</Text>
+      {confirm ? confirmComponent() : notConfirmComponent()}
     </SafeAreaView>
   );
 }
